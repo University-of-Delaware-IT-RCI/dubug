6772d9e3b518a46eb21923521df4f43610e84472
Jeffrey Frey <frey@udel.edu>
2020-01-17 21:52:58 -0500

    - Default to st_blocks * 512 instead of st_size - Add CLI flag to choose summation parameter (st_size, st_blocks, st_blocks * 512)

--- dubug.c
+++ dubug.c
@@ -17,46 +17,35 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
+#include <strings.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <getopt.h>
 #include <time.h>
 
+#ifndef ST_NBLOCKSIZE
+# ifdef S_BLKSIZE
+#  define ST_NBLOCKSIZE S_BLKSIZE
+# else
+#  define ST_NBLOCKSIZE 512
+# endif
+#endif
+
 //
 
 struct option cli_options[] = {
-        { "help",           no_argument,    NULL,   'h' },
-        { "quiet",          no_argument,    NULL,   'q' },
-        { "verbose",        no_argument,    NULL,   'v' },
-        { "human-readable", no_argument,    NULL,   'H' },
-        { "numeric",        no_argument,    NULL,   'n' },
-        { "progress",       no_argument,    NULL,   'p' },
-        { "unsorted",       no_argument,    NULL,   'S' },
-        { NULL,             0,              NULL,    0  }
+        { "help",               no_argument,        NULL,   'h' },
+        { "quiet",              no_argument,        NULL,   'q' },
+        { "verbose",            no_argument,        NULL,   'v' },
+        { "human-readable",     no_argument,        NULL,   'H' },
+        { "numeric",            no_argument,        NULL,   'n' },
+        { "progress",           no_argument,        NULL,   'p' },
+        { "progress-stride",    required_argument,  NULL,   'l' },
+        { "unsorted",           no_argument,        NULL,   'S' },
+        { "parameter",          required_argument,  NULL,   'P' },
+        { NULL,                 0,                  NULL,    0  }
     };
-const char *cli_options_str = "hqvHnpS";
-
-//
-
-const char*
-byte_count_to_string(
-    size_t      bytes
-)
-{
-    static const char* units[] = { "  B", "KiB", "MiB", "GiB", "TiB", "PiB", NULL };
-    static char outbuffer[64];
-
-    float       bytecount = (float)bytes;
-    int         unit = 0;
-
-    while ( bytecount > 1024.0 ) {
-        if ( ! units[unit + 1] ) break;
-        bytecount /= 1024.0;
-        unit++;
-    }
-    snprintf(outbuffer, sizeof(outbuffer), "%.2f %s", bytecount, units[unit]);
-    return outbuffer;
-}
+const char *cli_options_str = "hqvHnpl:SP:";
 
 //
 
@@ -66,7 +55,7 @@ typedef const char* (*entity_id_to_name_fn)(int32_t entity_id);
 
 typedef struct usage_record {
     int32_t     entity_id;
-    size_t      byte_usage;
+    uint64_t    byte_usage;
 
     struct usage_record *left[2], *middle[2], *right[2];
     struct usage_record *list;
@@ -95,19 +84,58 @@ enum {
     verbosity_debug = 4
 };
 
+enum {
+    parameter_actual = 0,
+    parameter_size = 1,
+    parameter_blocks = 2,
+    parameter_max = 3
+};
+
+const char* parameter_names[] = {
+    "actual",
+    "st_size",
+    "st_blocks",
+    NULL
+};
+
+#ifndef DEFAULT_PROGRESS_STRIDE
+#define DEFAULT_PROGRESS_STRIDE  10000
+#endif
+
 static usage_tree_t     *by_uid = NULL;
 static usage_tree_t     *by_gid = NULL;
-static size_t           total_usage = 0;
+static uint64_t         total_usage = 0;
 static uint64_t         item_count = 0;
 static int              verbosity = 1;
 static bool             should_show_human_readable = false;
 static bool             should_show_numeric_entity_ids = false;
 static bool             should_show_progress = false;
+static uint64_t         progress_stride = DEFAULT_PROGRESS_STRIDE;
 static bool             should_sort = true;
+static unsigned int     parameter = parameter_actual;
 
-#ifndef PROGRESS_LIMIT
-#define PROGRESS_LIMIT  8192
-#endif
+
+//
+
+const char*
+byte_count_to_string(
+    uint64_t    bytes
+)
+{
+    static const char* units[] = { "  B", "KiB", "MiB", "GiB", "TiB", "PiB", NULL };
+    static char outbuffer[64];
+
+    float       bytecount = (float)bytes;
+    int         unit = 0;
+
+    while ( bytecount > 1024.0 ) {
+        if ( ! units[unit + 1] ) break;
+        bytecount /= 1024.0;
+        unit++;
+    }
+    snprintf(outbuffer, sizeof(outbuffer), "%.2f %s", bytecount, units[unit]);
+    return outbuffer;
+}
 
 //
 
@@ -117,6 +145,43 @@ is_verbose(
 )
 {
     return ( level <= verbosity );
+}
+
+//
+
+bool
+set_parameter(
+    const char      *parameter_name
+)
+{
+    const char*     *P = parameter_names;
+    unsigned int    i = parameter_actual;
+    
+    while ( *P ) {
+        if ( strcasecmp(parameter_name, *P) == 0 ) {
+            parameter = i;
+            return true;
+        }
+        i++;
+        P++;
+    }
+    return false;
+}
+
+//
+
+bool
+set_progress_stride(
+    const char      *progress_stride_str
+)
+{
+    char                    *endptr = NULL;
+    unsigned long long int  value = strtoull(progress_stride_str, &endptr, 0);
+    
+    if ( ! value || (endptr == progress_stride_str) || (*endptr) ) return false;
+    
+    progress_stride = value;
+    return true;
 }
 
 //
@@ -299,18 +364,31 @@ __usage_record_summarize(
 
         if ( entity_to_name ) name = entity_to_name(root->entity_id);
 
-        if ( should_show_human_readable ) {
-            if ( name ) {
-                printf("%20s %24s (%6.2f%%)\n", name, byte_count_to_string(root->byte_usage), percentage);
-            } else {
-                printf("%20d %24s (%6.2f%%)\n", root->entity_id, byte_count_to_string(root->byte_usage), percentage);
-            }
-        } else {
-            if ( name ) {
-                printf("%20s %24llu (%6.2f%%)\n", name, (unsigned long long)root->byte_usage, percentage);
-            } else {
-                printf("%20d %24llu (%6.2f%%)\n", root->entity_id, (unsigned long long)root->byte_usage, percentage);
-            }
+        switch ( parameter ) {
+            case parameter_actual:
+            case parameter_size:
+                if ( should_show_human_readable ) {
+                    if ( name ) {
+                        printf("%20s %24s (%6.2f%%)\n", name, byte_count_to_string(root->byte_usage), percentage);
+                    } else {
+                        printf("%20d %24s (%6.2f%%)\n", root->entity_id, byte_count_to_string(root->byte_usage), percentage);
+                    }
+                } else {
+                    if ( name ) {
+                        printf("%20s %24llu (%6.2f%%)\n", name, (unsigned long long)root->byte_usage, percentage);
+                    } else {
+                        printf("%20d %24llu (%6.2f%%)\n", root->entity_id, (unsigned long long)root->byte_usage, percentage);
+                    }
+                }
+                break;
+            
+            case parameter_blocks:
+                if ( name ) {
+                    printf("%20s %24llu (%6.2f%%)\n", name, (unsigned long long)root->byte_usage, percentage);
+                } else {
+                    printf("%20d %24llu (%6.2f%%)\n", root->entity_id, (unsigned long long)root->byte_usage, percentage);
+                }
+                break;
         }
 
         // Go down the middle, too:
@@ -388,12 +466,26 @@ nftw_callback(
     }
 
     if ( is_verbose(verbosity_debug) && typeflag == FTW_D ) fprintf(stderr, "[DEBUG] %s\n", fpath);
-    total_usage += finfo->st_size;
+    
+    uint64_t        size;
+    
+    switch ( parameter ) {
+        case parameter_actual:
+            size = finfo->st_blocks * ST_NBLOCKSIZE;
+            break;
+        case parameter_size:
+            size = finfo->st_size;
+            break;
+        case parameter_blocks:
+            size = finfo->st_blocks;
+            break;
+    }
+    total_usage += size;
     item_count++;
     r = usage_tree_lookup_or_add(by_uid, finfo->st_uid);
-    if ( r ) r->byte_usage += finfo->st_size;
+    if ( r ) r->byte_usage += size;
     r = usage_tree_lookup_or_add(by_gid, finfo->st_gid);
-    if ( r ) r->byte_usage += finfo->st_size;
+    if ( r ) r->byte_usage += size;
 
     return 0;
 }
@@ -420,14 +512,28 @@ nftw_progress_callback(
     }
 
     if ( is_verbose(verbosity_debug) && typeflag == FTW_D ) fprintf(stderr, "[DEBUG] %s\n", fpath);
-    total_usage += finfo->st_size;
+    
+    uint64_t        size;
+    
+    switch ( parameter ) {
+        case parameter_actual:
+            size = finfo->st_blocks * ST_NBLOCKSIZE;
+            break;
+        case parameter_size:
+            size = finfo->st_size;
+            break;
+        case parameter_blocks:
+            size = finfo->st_blocks;
+            break;
+    }
+    total_usage += size;
     item_count++;
     r = usage_tree_lookup_or_add(by_uid, finfo->st_uid);
-    if ( r ) r->byte_usage += finfo->st_size;
+    if ( r ) r->byte_usage += size;
     r = usage_tree_lookup_or_add(by_gid, finfo->st_gid);
-    if ( r ) r->byte_usage += finfo->st_size;
+    if ( r ) r->byte_usage += size;
 
-    if ( (item_count % PROGRESS_LIMIT) == 0 ) {
+    if ( (item_count % progress_stride) == 0 ) {
         if ( is_verbose(verbosity_info) ) {
             fprintf(stderr, "[INFO]   %12llu items scanned...\n", (unsigned long long)item_count);
         } else {
@@ -448,16 +554,27 @@ usage(
             "usage -- [du] [b]y [u]ser and [g]roup\n\n"
             "    %s {options} <path> {<path> ..}\n\n"
             "  options:\n\n"
-            "    --help/-h              show this help info\n"
-            "    --verbose/-v           increase amount of output shown during execution\n"
-            "    --quiet/-q             decrease amount of output shown during execution\n"
-            "    --human-readable/-H    display usage with units, not as bytes\n"
-            "    --numeric/-n           do not resolve numeric uid/gid to names\n"
-            "    --progress/-p          display item-scanned counts as the traversal is\n"
-            "                           executing\n"
-            "    --unsorted/-S          do not sort by byte usage before summarizing\n"
+            "    --help/-h                show this help info\n"
+            "    --verbose/-v             increase amount of output shown during execution\n"
+            "    --quiet/-q               decrease amount of output shown during execution\n"
+            "    --human-readable/-H      display usage with units, not as bytes\n"
+            "    --numeric/-n             do not resolve numeric uid/gid to names\n"
+            "    --progress/-p            display item-scanned counts as the traversal is\n"
+            "                             executing\n"
+            "    --progress-stride/-l #   display progress every # items processed\n"
+            "                             (default: %llu)\n"
+            "    --unsorted/-S            do not sort by byte usage before summarizing\n"
+            "    --parameter/-P <param>   sizing field over which to sum:\n\n"
+            "                                 actual      bytes on disk (the default)\n"
+            "                                 size        nominal size (possibly sparse)\n"
+            "                                 blocks      block count\n"
+            "\n"
+            "  <path> can be an absolute or relative file system path to a directory or\n"
+            "  file (not very interesting), and for each <path> the traversal is repeated\n"
+            "  (rather than aggregating the sum over the paths).\n"
             "\n",
-            exe
+            exe,
+            (unsigned long long int)DEFAULT_PROGRESS_STRIDE            
         );
 }
 
@@ -496,16 +613,30 @@ main(
             case 'p':
                 should_show_progress = true;
                 break;
+            
+            case 'l':
+                if ( ! set_progress_stride(optarg) ) {
+                    if ( is_verbose(verbosity_error) ) fprintf(stderr, "[ERROR] Invalid argument to --progress-stride/-l: %s\n", optarg);
+                    exit(EINVAL);
+                }
+                break;
 
             case 'S':
                 should_sort = false;
+                break;
+            
+            case 'P':
+                if ( ! set_parameter(optarg) ) {
+                    if ( is_verbose(verbosity_error) ) fprintf(stderr, "[ERROR] Invalid argument to --parameter/-P: %s\n", optarg);
+                    exit(EINVAL);
+                }
                 break;
 
         }
     }
 
     // Increase our nice level (lowest priority possible, please):
-    nice(999);
+    if ( geteuid() != 0 ) nice(999);
 
     while ( (rc == 0) && (optind < argc) ) {
         const char      *root_path = argv[optind];
@@ -530,25 +661,38 @@ main(
         if ( is_verbose(verbosity_info) ) {
             double      seconds = (end_time.tv_sec - start_time.tv_sec) + 1e-9 * (end_time.tv_nsec - start_time.tv_nsec);
 
-            fprintf(stderr, "[INFO] Completed traversal of %s in %.3f seconds\n", root_path, seconds);
+            fprintf(stderr, "[INFO] Completed traversal of %s\n", root_path);
+            fprintf(stderr, "[INFO]   %llu files/directories in %.3f seconds\n", (unsigned long long int)item_count, seconds);
             fprintf(stderr, "[INFO]   %12.0f files/directories per second\n", (double)item_count / seconds);
         }
         if ( is_verbose(verbosity_error) && (rc != 0) ) fprintf(stderr, "[ERROR] Directory walk exited early due to internal failure\n");
 
         // Sumarize:
         printf("Total usage:\n");
-        if ( should_show_human_readable ) {
-            printf(
-                   "%20s %24s\n",
-                   "", byte_count_to_string(total_usage)
-                );
-        } else {
-            printf(
-                   "%20s %24llu\n",
-                   "", (unsigned long long)total_usage
-                );
+        switch ( parameter ) {
+        
+            case parameter_actual:
+            case parameter_size:
+                if ( should_show_human_readable ) {
+                    printf(
+                           "%20s %24s\n",
+                           "", byte_count_to_string(total_usage)
+                        );
+                } else {
+                    printf(
+                           "%20s %24llu\n",
+                           "", (unsigned long long)total_usage
+                        );
+                }
+                break;
+            
+            case parameter_blocks:
+                printf(
+                       "%20s %24llu\n",
+                       "", (unsigned long long)total_usage
+                    );
+                break;
         }
-
         if ( should_sort ) {
             if ( is_verbose(verbosity_debug) ) fprintf(stderr, "[DEBUG] Sorting by-uid tree by byte usage\n");
             usage_tree_sort_by_byte_usage(by_uid);
